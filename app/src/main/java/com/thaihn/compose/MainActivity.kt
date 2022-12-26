78659809edbb1621a7d7f891e4c03922b6cc9070
Hoang Ngoc Thai <ngocthaihn96@gmail.com>
2022-12-26 22:52:47 +0700

    Custom show card (#2)

--- app/src/main/java/com/thaihn/compose/MainActivity.kt
+++ app/src/main/java/com/thaihn/compose/MainActivity.kt
@@ -3,13 +3,14 @@ package com.thaihn.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,11 +18,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.thaihn.compose.ui.theme.MyApplicationTheme
 import com.thaihn.compose.ui.theme.robotoFamily
 
@@ -35,40 +39,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CustomText("Android")
+                    Contact(name = "Ngọc Thái", position = "Android developer")
                 }
             }
         }
     }
 }
 
+data class Contact(
+    val name: String? = null,
+
+    val position: String? = null,
+
+    val image: String? = null
+)
+
 @Composable
-fun CustomText(name: String) {
-    Column(
-        modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth()
-            .background(color = Color.Cyan)
-    ) {
-        Text(
-            text = "Hi $name!",
-            fontSize = 16.sp,
-            color = Color.Blue,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
+fun ItemContact(contact: Contact) {
+    Row(modifier = Modifier.padding(10.dp)) {
+        if (contact.image != null) {
+            AsyncImage(
+                model = contact.image,
+                contentDescription = stringResource(id = R.string.contentDescriptionAvatar)
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.tree),
+                contentDescription = stringResource(id = R.string.contentDescriptionAvatar),
+                modifier = Modifier.size(50.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Column(
             modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally)
-                .padding(top = 10.dp),
-            fontFamily = robotoFamily,
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            text = "Hoang Ngoc Thai",
-            fontSize = 12.sp,
-            fontFamily = robotoFamily,
-            fontWeight = FontWeight.Medium,
-            modifier = Modifier.padding(start = 16.dp, bottom = 16.dp)
-        )
+                .padding(start = 10.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .align(Alignment.CenterVertically),
+        ) {
+            Text(
+                text = contact.name ?: "",
+                fontSize = 16.sp,
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold,
+                fontFamily = robotoFamily,
+            )
+            Text(
+                text = contact.position ?: "",
+                fontSize = 12.sp,
+                fontFamily = robotoFamily,
+                fontWeight = FontWeight.Medium,
+            )
+        }
     }
 }
 
@@ -76,6 +98,6 @@ fun CustomText(name: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        CustomText("Android")
+        ItemContact(Contact(name = "Ngoc Thai", position = "Android developer"))
     }
 }
