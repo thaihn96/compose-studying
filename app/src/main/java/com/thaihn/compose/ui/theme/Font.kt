74affec2e462d7d3881b4cc12e38d77bcc713cd0
Hoang Ngoc Thai <ngocthaihn96@gmail.com>
2022-12-26 17:11:08 +0700

    Create text and style (#1)

---
+++ app/src/main/java/com/thaihn/compose/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package com.thaihn.compose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.thaihn.compose.R
+
+/**
+ * Created by Thaihn on 26/12/2022.
+ */
+val robotoFamily = FontFamily(
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_medium, FontWeight.Medium)
+)
